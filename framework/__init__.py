e49f74b58feea9b038a5a45ba4c1fdda3949eb9b
GektorPrime <60014270+GektorPrime@users.noreply.github.com>
2024-05-24 13:25:41 -0400

    Update __init__.py TARGET_ENV

--- framework/__init__.py
+++ framework/__init__.py
@@ -1,8 +1,9 @@
 import configparser
 from pathlib import Path
+from os import environ
 
 __file_dir = Path(__file__).resolve().parent.parent
 __automationexercise_config = configparser.ConfigParser()
 __automationexercise_config.read(__file_dir / 'infrastructure' / 'automationexercise_config.ini')
-TARGET_ENV = 'prd'
+TARGET_ENV = environ.get('TARGET_ENV') or 'prd'  # takes environmental variable if exists or defaults to 'prd'
 AUTOMATIONEXERCISE_CONFIG = __automationexercise_config[TARGET_ENV]
